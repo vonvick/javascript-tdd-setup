e378fce77b3696c97967eba39f92c7ced93eebc5
johnez1987 <vonvikky@gmail.com>
2016-08-18 17:49:11 +0100

    added more tests cases to tests.js

--- tests.js
+++ tests.js
@@ -16,7 +16,7 @@ describe("Note creation works properly", function() {
 
 
   it("assigns author based on the parameter supplied in the constructor", function() {
-    assert(noteapp.name === "Victor");
+    assert(noteapp.author === "Victor");
   });
 
 });
@@ -29,7 +29,6 @@ describe("NoteApplication works properly", function() {
   beforeEach(function() {
     noteapp = new NotesApplication("Victor");
     note = new Note("How are you", "Victor");
-    // note = "I am a boy";
   });
 
   it("return an empty list of notes", function() {
@@ -39,6 +38,7 @@ describe("NoteApplication works properly", function() {
   it("return an empty list of notes", function() {
     var lists = noteapp.listNotes();
     assert.isString(lists, "No note in the list");
+    assert(lists === "No note in the list"):
   });
 
   it("increments the note list as notes are added", function() {
@@ -51,6 +51,7 @@ describe("NoteApplication works properly", function() {
     noteapp.create(note2);
     var getNote = noteapp.getNote(1);
     assert.isDefined(getNote);
+    assert(getNote === "I love andela");
   })
 
   it("returns a text that says that the note can't be found", function() {
@@ -76,22 +77,23 @@ describe("NoteApplication works properly", function() {
     var note3 = noteapp.create("I like to learn");
     var deleted = noteapp.delete(3);
     assert.isString(deleted)
-  })
+    assert(deleted === "Could not find the file to delete");
+  });
 
   it("returns the search result for the search text ", function() {
     var search =noteapp.search("are")
     assert.isNotNull(search)
-  })
+  });
 
   it("returns a string that says Could not find text: ", function() {
     var search =noteapp.search("talk");
     assert(search === "Could not find your search text")
-  })
+  });
 
   it("returns a string that says: ", function() {
     var search =noteapp.search("");
     assert(search === "You did not ente a search text")
-  })
+  });
 
   it("returns a new value for the edited text: ", function() {
     var note2 = noteapp.create(note);
@@ -99,8 +101,20 @@ describe("NoteApplication works properly", function() {
     var note4 = noteapp.create("This is the bootcamp");
     noteapp.create(note2);
     var edit =noteapp.edit(2, "This is andela");
-    assert.isDefined(edit)
-  })
+    assert.isDefined(edit);
+    assert(edit === "This is andela");
+  });
+
+  it("returns a new value for the edited text: ", function() {
+    var note2 = noteapp.create(note);
+    var note3 = noteapp.create("I like to learn");
+    var note4 = noteapp.create("This is the bootcamp");
+    noteapp.create(note2);
+    noteapp.create(note3);
+    noteapp.create(note4);
+    var edit =noteapp.edit(2, "This is andela");
+    assert.isString(edit);
+  });
 
 
 })

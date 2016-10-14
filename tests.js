1b57b2196888b9f6ee99e6b1c7fad662367fe7ba
vonvick <victor.nwaiwu9@gmail.com>
2016-10-14 18:03:58 +0100

    create test case for isPrime() function

--- tests.js
+++ tests.js
@@ -27,4 +27,25 @@ describe("Test that returns the sum of prime numbers from 0 to the number", func
   it("should return false when a negative number is supplied", function() {
     assert(lib.sumOfPrimes(-20) == false);
   });
+  it("should return false when an empty string is supplied", function() {
+    assert(lib.sumOfPrimes("") == false);
+  });
+});
+
+describe("Test that checks if a number is prime or not", function() {
+  it("should return true for numbers that are prime numbers",  function() {
+    assert(lib.isPrime(5) == true);
+  });
+  it("should return false for numbers that are not prime numbers",  function() {
+    assert(lib.isPrime(15) == false);
+  });
+  it("should return false for numbers less than 2",  function() {
+    assert(lib.isPrime(1) == false);
+  });
+  it("should return false for even numbers greater than 2",  function() {
+    assert(lib.isPrime(16) == false);
+  });
+  it("should return true for 2",  function() {
+    assert(lib.isPrime(2) == true);
+  });
 });

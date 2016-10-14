76aea75f3eab0a8159df2cd83cc1c724ed92aeb0
vonvick <victor.nwaiwu9@gmail.com>
2016-10-14 17:46:13 +0100

    write first test case for sumOfPrimes function

--- tests.js
+++ tests.js
@@ -5,20 +5,8 @@ var assert = chai.assert;
 
 var lib = require('./lib/sum_of_primes.js');
 
-describe("Test that constants are computed properly", function() {
-  it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(2, 5), { a: 10, n: 4 })
-    );
-  });
-  it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(1, 2), { a: 2, n: 1 })
-    );
-  });
-  it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(4, 2), { a: 8, n: 1 })
-    );
+describe("Test that returns the sum of prime numbers from 0 to the number", function() {
+  it("should return false for numbers less than 2", function() {
+    assert(lib.sumOfPrimes(1) == false);
   });
 });

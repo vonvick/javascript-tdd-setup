a34154a90f5652a3af420d157ffdf17b95ccd675
vonvick <victor.nwaiwu9@gmail.com>
2016-10-14 17:49:05 +0100

    check test case when a number is given as arguments

--- tests.js
+++ tests.js
@@ -9,4 +9,7 @@ describe("Test that returns the sum of prime numbers from 0 to the number", func
   it("should return false for numbers less than 2", function() {
     assert(lib.sumOfPrimes(1) == false);
   });
+  it("should return 2 if number is equal to 2", function() {
+    assert(lib.sumOfPrimes(2) == 2);
+  });
 });
